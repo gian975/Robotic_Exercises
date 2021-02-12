9153528f88d8b5c1c2464090621dfbbaa4c41439
gian975 <g.damore14@studenti.unisa.it>
2021-02-12 09:11:57 +0100

    update exercise 3

--- Exercise3/build/fanuc_moveit_config_no_limit/catkin_generated/setup_cached.sh
+++ Exercise3/build/fanuc_moveit_config_no_limit/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/gianluca/Desktop/Università/Robotica/project_delivery/Robotic_Exercises/Exercise3/devel/.private/fanuc_moveit_config_no_limit:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/gianluca/Desktop/Università/Robotica/project_delivery/Robotic_Exercises/Exercise3/devel/.private/fanuc_moveit_config_no_limit/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/gianluca/Desktop/Università/Robotica/project_delivery/Robotic_Exercises/Exercise3/devel/.private/fanuc_moveit_config_no_limit/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/gianluca/Desktop/Università/Robotica/project_delivery/Robotic_Exercises/Exercise3/build/fanuc_moveit_config_no_limit'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/gianluca/Desktop/Università/Robotica/project_delivery/Robotic_Exercises/Exercise3/devel/.private/fanuc_moveit_config_no_limit/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/gianluca/Desktop/Università/Robotica/project_delivery/Robotic_Exercises/Exercise3/src/fanuc_moveit_config_no_limit:$ROS_PACKAGE_PATH"
